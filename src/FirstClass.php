cade3041164ebb9c8ecaa2dc1446585fc9ba1f7e
Jay5 <1392320795@qq.com>
2023-11-05 17:45:17 +0800

    composer-demo init

---
+++ src/FirstClass.php
@@ -0,0 +1,10 @@
+<?php
+namespace Test\Composerea;
+
+class FirstClass
+{
+    public function __construct()
+    {
+        echo '我是阿猫';
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'test/composerea',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'test/composerea' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
